eb94707dbc0d8a9fea86cb00ac77f6ad2ccbddf4
Jason Phan <jase.phan@mail.utoronto.ca>
2026-02-14 02:52:28 -0500

    added check magic and partial header handler

--- RazzTFTRender/src/decoder/decoder.cpp
+++ RazzTFTRender/src/decoder/decoder.cpp
@@ -1,4 +1,5 @@
 #include "decoder.hpp"
+#include "frame.hpp"
 
 void Decoder::rgb565_to_rgb888(uint16_t rgb565, uint8_t &r, uint8_t &g, uint8_t &b) {
     r = ((rgb565 >> 11) & R_MASK) << 3;
@@ -6,6 +7,74 @@ void Decoder::rgb565_to_rgb888(uint16_t rgb565, uint8_t &r, uint8_t &g, uint8_t 
     b = (rgb565 & B_MASK) << 3;
 }
 
-void Decoder::feed(uint8_t bytes) {
+void Decoder::feed(uint8_t byte) {
+    switch (state) {
+        case DecodeState::FIND_SYNC: handle_sync(byte); break;
+        case DecodeState::READ_HEADER: handle_header(byte); break;
+        case DecodeState::READ_PAYLOAD: handle_payload(byte); break;
+        case DecodeState::READ_PAYLOAD_CHECKSUM: handle_checksum(byte); break;
+    }
+}
+
+// Sync magic have 2 bytes, we transition at 1 
+// Also dont wanna use increment before check cuz slow (hardware)
+void Decoder::handle_sync(uint8_t byte) {
+    if ((bytes_read == 0 && byte != (MAGIC_BYTES & 0xFF)) ||
+            (bytes_read == 1 && byte != (MAGIC_BYTES >> BITS_IN_BYTE))) {
+        bytes_read = 0;
+        return;
+    } else if (bytes_read == 1) {
+        state = DecodeState::READ_HEADER;
+        bytes_read = 0;
+        return;
+    }
+    bytes_read++;
+    return;
+}
+
+// Exclude magic, we have 
+// 2 bytes for width -> up to 1
+// 2 bytes for height -> up to 3
+// 2 bytes checksum -> up to 5
+// Logic
+void Decoder::handle_header(uint8_t byte) {
+    // WIDTH
+    if (bytes_read == 0) {
+        width = (byte & BYTE_MASK);
+    } else if (bytes_read == 1) {
+        width |= (byte << BITS_IN_BYTE);
+    } 
+
+    // HEIGHT
+    else if (bytes_read == 2) {
+        height = (byte & BYTE_MASK);
+    } else if (bytes_read == 3) {
+        height |= (byte << BITS_IN_BYTE);
+    } 
+
+    // CHECKSUM
+    else if (bytes_read == 4) {
+        checksum = (byte & BYTE_MASK);
+    } else if (bytes_read == 5) {
+        checksum |= (byte << BITS_IN_BYTE);
+        // Change state 
+        state = DecodeState::READ_PAYLOAD;
+        bytes_read = 0;
+        return;
+    }
+    
+    // CHECKSUM ACCUMULATION 
+    if (bytes_read < 4) {
+        crc.add(byte);
+    }
+    bytes_read++;
+    return;
+}
+
+void Decoder::handle_payload(uint8_t byte) {
+    
+}
+
+void Decoder::handle_checksum(uint8_t byte) {
 
 }
--- RazzTFTRender/src/decoder/decoder.hpp
+++ RazzTFTRender/src/decoder/decoder.hpp
@@ -8,17 +8,34 @@
 #define R_MASK 0x1F
 #define G_MASK 0x3F
 #define B_MASK 0x1F
+#define BITS_IN_BYTE 8
+#define BYTE_MASK 0xFF
 
 
 class Decoder {
 public:
-    Decoder();
+    Decoder() : state(DecodeState::FIND_SYNC), width(0), 
+    height(0), img_size(0) {}
     void feed(uint8_t byte);
 
 
 private: 
     void rgb565_to_rgb888(uint16_t rgb565, uint8_t &r, uint8_t &g, uint8_t &b);
+
+    void handle_sync(uint8_t byte);
+    void handle_header(uint8_t byte);
+    void handle_payload(uint8_t byte);
+    void handle_checksum(uint8_t byte);
+
     FrameHeader header;
     CRC16 crc;
     uint8_t buffer[BUFFER_SIZE];
+
+    uint16_t width;
+    uint16_t height;
+    size_t img_size;
+    uint16_t checksum;
+
+    DecodeState state;
+    size_t bytes_read;
 };
