b1184b1a8473f7c66b67db2d461a20b34155e5c8
Jason Phan <jasonphan210@gmail.com>
2026-08-05 15:36:52 -0400

    add some generic for bin op

--- razz-compiler/src/ir/ssa/optimizer/constant_folding.rs
+++ razz-compiler/src/ir/ssa/optimizer/constant_folding.rs
@@ -1,21 +1,65 @@
-use crate::ir::ssa::{optimizer::Optimization, ssa::{SSABlock, SSAFunction, SSAInstruction, SSAOperand, SSAProgram}};
+use std::ops::{Add, Div, Mul, Sub};
+
+use crate::{ast::expression::{BinOpKind, Literal}, ir::{Temp, ssa::{optimizer::Optimization, ssa::{SSABlock, SSAFunction, SSAInstruction, SSAOperand, SSAProgram}}}};
 
 pub struct ConstantFolding;
 
-//fn expand_bin_op(target: )
+fn expand_bin_op_instr(target: &Temp, lhs: &SSAOperand, op: &BinOpKind, rhs: &SSAOperand) -> Option<SSAInstruction> {
+    match (lhs, rhs) {
+        (SSAOperand::Const(l1), SSAOperand::Const(l2)) => {
+            let folded = expand_literal(l1, op, l2);
+            todo!()
+        },
+        (_, _) => None, 
+    }
+}
+
+fn expand_literal(lhs: &Literal, op: &BinOpKind, rhs: &Literal) -> Literal {
+    match (lhs, op, rhs) {
+        (Literal::Int(i1), BinOpKind::Add, Literal::Int(i2)) 
+        | (Literal::Int(i1), BinOpKind::Sub, Literal::Int(i2)) 
+        | (Literal::Int(i1), BinOpKind::Div, Literal::Int(i2)) 
+        | (Literal::Int(i1), BinOpKind::Mult, Literal::Int(i2)) 
+        => 
+            Literal::Int(expand_bin_op_arithmetic(*i1, op, *i2)),
+
+        (Literal::Float(f1), BinOpKind::Add, Literal::Float(f2)) 
+        | (Literal::Float(f1), BinOpKind::Sub, Literal::Float(f2)) 
+        | (Literal::Float(f1), BinOpKind::Div, Literal::Float(f2)) 
+        | (Literal::Float(f1), BinOpKind::Mult, Literal::Float(f2))  
+        => Literal::Float(expand_bin_op_arithmetic(*f1, op, *f2)),
+        _ => todo!()
+    }
+}
+
+/// This is the most ive written with rust generic...
+fn expand_bin_op_arithmetic<T>(lhs: T, op: &BinOpKind, rhs: T) -> T
+where 
+    T: Add<Output = T> 
+    + Sub<Output = T>
+    + Div<Output = T> 
+    + Mul<Output = T> {
+    match op {
+        BinOpKind::Add => lhs + rhs, 
+        BinOpKind::Sub => lhs - rhs, 
+        BinOpKind::Div => lhs / rhs, 
+        BinOpKind::Mult => lhs * rhs, 
+        _ => unreachable!("Caller deal with it, aka me")
+    }
+}
 
 impl ConstantFolding {
     fn constant_fold_block(&self, block: &mut SSABlock) -> bool {
         let mut mutated = false;
-        for instr in &block.instrs {
-            mutated = mutated || match instr {
+        for i in 0..block.instrs.len() {
+            mutated = mutated || match &block.instrs[i] {
                 SSAInstruction::BinOp { target, lhs, op, rhs } => {
-                    match (lhs, rhs) {
-                        (SSAOperand::Const(l1), SSAOperand::Const(l2)) => {
-                            true
-                        }
-                        (_, _) => false
+                    let instr = expand_bin_op_instr(target, lhs, op, rhs);
+                    let instr_mutated = instr.is_some();
+                    if let Some(instr) = instr {
+                        block.instrs[i] = instr;
                     }
+                    instr_mutated
                 },
                 SSAInstruction::UnOp { target, op, value } => {
                     false
