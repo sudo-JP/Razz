dd29ecfe8aad1dde464a2f1cc1da0b1199b5e81d
Jason Phan <jasonphan210@gmail.com>
2026-08-06 15:06:20 -0400

    finish bin op const fold

--- razz-compiler/src/ir/ssa/optimizer/constant_folding.rs
+++ razz-compiler/src/ir/ssa/optimizer/constant_folding.rs
@@ -1,4 +1,4 @@
-use std::ops::{Add, Div, Mul, Sub};
+use std::ops::{Add, Div, Mul, Sub, };
 
 use crate::{ast::expression::{BinOpKind, Literal}, ir::{Temp, ssa::{optimizer::Optimization, ssa::{SSABlock, SSAFunction, SSAInstruction, SSAOperand, SSAProgram}}}};
 
@@ -8,14 +8,17 @@ fn expand_bin_op_instr(target: &Temp, lhs: &SSAOperand, op: &BinOpKind, rhs: &SS
     match (lhs, rhs) {
         (SSAOperand::Const(l1), SSAOperand::Const(l2)) => {
             let folded = expand_literal(l1, op, l2);
-            todo!()
+            Some(SSAInstruction::Copy { target: *target, value: SSAOperand::Const(folded) })
         },
         (_, _) => None, 
     }
 }
 
+// This is hell to type btw 
 fn expand_literal(lhs: &Literal, op: &BinOpKind, rhs: &Literal) -> Literal {
     match (lhs, op, rhs) {
+        // Int 
+        // Arithmetic
         (Literal::Int(i1), BinOpKind::Add, Literal::Int(i2)) 
         | (Literal::Int(i1), BinOpKind::Sub, Literal::Int(i2)) 
         | (Literal::Int(i1), BinOpKind::Div, Literal::Int(i2)) 
@@ -23,12 +26,49 @@ fn expand_literal(lhs: &Literal, op: &BinOpKind, rhs: &Literal) -> Literal {
         => 
             Literal::Int(expand_bin_op_arithmetic(*i1, op, *i2)),
 
+        // Comparison
+        (Literal::Int(i1), BinOpKind::Lt, Literal::Int(i2)) 
+        | (Literal::Int(i1), BinOpKind::Le, Literal::Int(i2)) 
+        | (Literal::Int(i1), BinOpKind::Gt, Literal::Int(i2)) 
+        | (Literal::Int(i1), BinOpKind::Ge, Literal::Int(i2)) 
+        | (Literal::Int(i1), BinOpKind::Eq, Literal::Int(i2)) 
+        | (Literal::Int(i1), BinOpKind::Neq, Literal::Int(i2)) 
+        => 
+            Literal::Bool(expand_bin_op_comparison(*i1, op, *i2)),
+
+        // Float
+        // Arithmetic
         (Literal::Float(f1), BinOpKind::Add, Literal::Float(f2)) 
         | (Literal::Float(f1), BinOpKind::Sub, Literal::Float(f2)) 
         | (Literal::Float(f1), BinOpKind::Div, Literal::Float(f2)) 
         | (Literal::Float(f1), BinOpKind::Mult, Literal::Float(f2))  
         => Literal::Float(expand_bin_op_arithmetic(*f1, op, *f2)),
-        _ => todo!()
+
+        // Comparison
+        (Literal::Float(f1), BinOpKind::Lt, Literal::Float(f2)) 
+        | (Literal::Float(f1), BinOpKind::Le, Literal::Float(f2)) 
+        | (Literal::Float(f1), BinOpKind::Gt, Literal::Float(f2)) 
+        | (Literal::Float(f1), BinOpKind::Ge, Literal::Float(f2)) 
+        | (Literal::Float(f1), BinOpKind::Eq, Literal::Float(f2)) 
+        | (Literal::Float(f1), BinOpKind::Neq, Literal::Float(f2)) 
+        => 
+            Literal::Bool(expand_bin_op_comparison(*f1, op, *f2)),
+
+        // Bool
+        // Comparison
+        (Literal::Bool(b1), BinOpKind::Eq, Literal::Bool(b2)) 
+        | (Literal::Bool(b1), BinOpKind::Neq, Literal::Bool(b2)) 
+        => 
+            Literal::Bool(expand_bin_op_comparison(*b1, op, *b2)),
+
+        (Literal::Bool(b1), BinOpKind::And, Literal::Bool(b2))  
+        => Literal::Bool(*b1 && *b2),
+        (Literal::Bool(b1), BinOpKind::Or, Literal::Bool(b2)) 
+        => Literal::Bool(*b1 || *b2),
+
+        (Literal::String(s1), BinOpKind::Add, Literal::String(s2))
+        => Literal::String(format!("{s1}{s2}")),
+        _ => unreachable!("semantic problem")
     }
 }
 
@@ -45,6 +85,21 @@ where
         BinOpKind::Div => lhs / rhs, 
         BinOpKind::Mult => lhs * rhs, 
         _ => unreachable!("Caller deal with it, aka me")
+    }
+}
+
+fn expand_bin_op_comparison<T>(lhs: T, op: &BinOpKind, rhs: T) -> bool
+where 
+    T: PartialOrd
+    + PartialEq {
+    match op {
+        BinOpKind::Lt => lhs < rhs, 
+        BinOpKind::Le => lhs <= rhs, 
+        BinOpKind::Gt => lhs > rhs, 
+        BinOpKind::Ge => lhs >= rhs, 
+        BinOpKind::Eq => lhs == rhs, 
+        BinOpKind::Neq => lhs != rhs, 
+        _ => unreachable!("Same err as above")
     }
 }
 
@@ -67,7 +122,6 @@ impl ConstantFolding {
                 _ => false,
             };
         }
-
         mutated
     }
 
